59090a444a2b946f72bac365f3203f71bed09ac8
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-29 17:29:33 +0530

    Function to roll the dice and update their values

---
+++ script.js
@@ -0,0 +1,32 @@
+// Selecting elements from the DOM
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRoundText = document.getElementById("current-round");
+const currentRoundRollsText = document.getElementById("current-round-rolls");
+const totalScoreText = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+// Game state variables
+let diceValuesArr = [];
+let isModalShowing = false;
+let score = 0;
+let totalScore = 0;
+let round = 1;
+let rolls = 0;
+
+// Function to roll the dice and update their values
+const rollDice = () => {
+  diceValuesArr = [];
+
+  // Generate random values for the dice
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  }
+
+  
